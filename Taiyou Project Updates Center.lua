7f03009a3999fd53eb074484c13fe22a4a260749
Taiyouscape <43681888+Taiyouscape@users.noreply.github.com>
2019-04-07 21:25:21 +0700

    Taiyou Project Updates Center v1.1
    Added Changelog

--- Taiyou Project Updates Center.lua
+++ Taiyou Project Updates Center.lua
@@ -1,8 +1,8 @@
 -- Taiyouscape --
 goto s
-
+-- Menu --
 ::s::
-menu = gg.choice ({'[✔️]Ｓｔａｂｌｅ','[☢️]Ｂｅｔａ'}, nill, "Taiyou Project Updates Center v1.0")
+menu = gg.choice ({'[✔️]Ｓｔａｂｌｅ','[☢️]Ｂｅｔａ'}, nill, "Taiyou Project Updates Center v1.1")
 
 if menu == 1 then goto stable end
 if menu == 2 then goto beta end
@@ -13,12 +13,14 @@ os.exit()
 menusec = gg.choice({
 "[1️⃣]Stable Ｉｃｏｎ Ｍｉｎｉｍａｐ",
 "[2️⃣]Stable Ｊｕｎｇｌｅ Ｒａｄａｒ",
+"[⁉️]Changelog Stable",
 "[◀️]"
 }, nil, "Stable will updated everyweek!.")
 if menusec == nil then else
 if menusec == 1 then goto ims end
 if menusec == 2 then goto jrs end
-if menusec == 3 then goto s end
+if menusec == 3 then goto cs end
+if menusec == 4 then goto s end
 end
 os.exit()
 
@@ -26,20 +28,23 @@ os.exit()
 menusec = gg.choice({
 "[1️⃣]Beta Ｉｃｏｎ Ｍｉｎｉｍａｐ",
 "[2️⃣]Beta Ｊｕｎｇｌｅ Ｒａｄａｒ",
+"[⁉️]Changelog Beta",
 "[◀️]"
 }, nil, "Beta will updated everyday!.")
 if menusec == nil then else
 if menusec == 1 then goto imb end
 if menusec == 2 then goto jrb end
-if menusec == 3 then goto s end
+if menusec == 3 then goto cb end
+if menusec == 4 then goto s end
 end
 os.exit()
 
-
+-- Script --
+-- Direct --
 ::ims::
 TG = gg.makeRequest("https://raw.githubusercontent.com/Taiyouscape/TaiyouProjects/Release/%5BStable%5DIconOnly.lua").content
 if not TG then
-gg.alert('Something Went Wrong')
+gg.alert('Turn on Internet!')
 os.exit()
 else﻿
 pcall(load(TG))
@@ -49,7 +54,7 @@ os.exit()
 ::jrs::
 TG = gg.makeRequest("https://raw.githubusercontent.com/Taiyouscape/TaiyouProjects/Release/%5BStable%5DJungleOnly.lua").content
 if not TG then
-gg.alert('Something Went Wrong')
+gg.alert('Turn on Internet!')
 os.exit()
 else﻿
 pcall(load(TG))
@@ -59,7 +64,7 @@ os.exit()
 ::imb::
 TG = gg.makeRequest("https://raw.githubusercontent.com/Taiyouscape/TaiyouProjects/Release/%5BBeta%5DIconOnly.lua").content
 if not TG then
-gg.alert('Something Went Wrong')
+gg.alert('Turn on Internet!')
 os.exit()
 else﻿
 pcall(load(TG))
@@ -69,9 +74,19 @@ os.exit()
 ::jrb::
 TG = gg.makeRequest("https://raw.githubusercontent.com/Taiyouscape/TaiyouProjects/Release/%5BBeta%5DJungleOnly.lua").content
 if not TG then
-gg.alert('Something Went Wrong')
+gg.alert('Turn on Internet!')
 os.exit()
 else﻿
 pcall(load(TG))
 end﻿
 os.exit()
+
+-- Changelog --
+::cs::
+gg.alert('Taiyou Project Script v1.2stable0504190931\n\n- Reworked some Value!')
+os.exit()
+
+::cb::
+gg.alert('Taiyou Project Script v1.3beta0704191109\nUNSTABLE VERSION‼️\n\n- Fix some value\n- Added new algorithm for faster scanning")
+os.exit()
+  

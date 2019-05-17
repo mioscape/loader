66f999bf8d16f67fe3ee280c3588bb1f848e94fd
Raphielsama <43681888+Raphielsama@users.noreply.github.com>
2019-05-17 15:03:07 +0700

    Delete RaphielLoader.lua

--- RaphielLoader.lua
+++
@@ -1,8 +0,0 @@
-TG = gg.makeRequest("https://raw.githubusercontent.com/Raphielsama/RaphielProject/master/RaphielLoaderUpdates.lua").content
-if not TG then
-gg.alert('Turn on Internet!')
-os.exit()
-else﻿
-pcall(load(TG))
-end﻿
-os.exit()

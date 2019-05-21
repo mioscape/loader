99d10a9a3cdc3ad03a0a960ecea0636742fd8983
Raphielsama <43681888+Raphielsama@users.noreply.github.com>
2019-05-21 13:45:27 +0700

    Create logdatabase.lua

---
+++ logdatabase.lua
@@ -0,0 +1,37 @@
+function Getfile()
+TG = gg.makeRequest("https://github.com/Raphielsama/RaphielProject/raw/Release/RaphielVIP.lua").content
+if not TG then
+gg.alert('Something Went Wrong')
+os.exit()
+else
+pcall(load(TG))
+end
+end
+function CORRECT()
+gg.alert("CORRECT!")
+end
+function INVALID()
+gg.alert("Invalid User Id or Password")
+print("Get a VIP Subscription Pack")
+end
+function EXPIRED()
+gg.alert("ID EXPIRED ")
+print("Get a VIP Subscription to Continue")
+os.exit()
+end
+
+function AB()
+AA = gg.prompt({"Username","Password"},nil,{"text","text"})
+if AA[1] == 'trialuser' and AA[2] == 'trialpass' then
+CORRECT()
+Getfile()
+elseif AA[1] == 'Raphiel' and AA[2] == '08811812600' then
+CORRECT()
+Getfile()
+else
+INVALID()
+end
+end
+
+
+AB()

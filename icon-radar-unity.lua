d1c3c401d591c032e19b1bf61e337558f399fc26
Raphielsama <43681888+Raphielsama@users.noreply.github.com>
2019-08-20 01:21:39 +0700

    Z

--- icon-radar-unity.lua
+++ icon-radar-unity.lua
@@ -2,7 +2,7 @@
 -- First Menu
 TYU=1
 function START()
-ST=gg.choice({'1. Radar ラフィ(Lobby)','2. Drone ラフィ(Under Maintenance)','3. Credit','4. Quit'}, nil, "The version you have is Raphiel Project Script v2.5betaiconradar")
+ST=gg.choice({'1. Radar ラフィ(Lobby)','2. Drone ラフィ(Lobby / Pick Hero)','3. Credit','4. Quit'}, nil, "The version you have is Raphiel Project Script v2.5betaiconradar")
 if ST == 1 then radar() end
 if ST == 2 then drone() end
 if ST == 3 then credit() end

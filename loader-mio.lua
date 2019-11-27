7d2c69c192133740f116062ea218af7947d06bf6
Mioscape <43681888+mioscape@users.noreply.github.com>
2019-11-27 21:59:22 +0700

    loader-mio
    Changelogs v2.6.1:
    - Remove Quit Dialog
    - Fix Strings

--- loader-mio.lua
+++ loader-mio.lua
@@ -1,42 +1,28 @@
 -- mioscape
-gg.alert('Mioscape is not responsible for banned your account!\n\nMioscape tidak bertanggung jawab Atas ke banned nya akun anda!')
+gg.alert('Mioscape is not responsible for banned your account!')
 gg.toast('Donation List:\n1. Mdwis\n2. Dimas')
--- Menu
+
 goto s
 
 ::s::
 menu = gg.choice({
 "// MLBB Icon Radar - Unity 2017 - Recommended //",
-"// MLBB Normal Radar - Unity 2017 //",
-"// Personal //",
-"// Support Mio //",
+"// MLBB Normal Radar - Unity 2017 - Unstable //",
+"// MLBB Beta Radar - Unity 2017 - Developer Only //",
 "// Quit //"
-}, nil, "Mio Loader v2.6 | Made with 💖 Mio Natsume")
+}, nil, "Mio Loader v2.6.1 | Made with 💖 Mio Natsume")
 if menu == nil then else
 if menu == 1 then goto ims end
 if menu == 2 then goto nrs end
 if menu == 3 then goto beta end
-if menu == 4 then goto su end
-if menu == 5 then goto q end
-end
-os.exit()
--- Submenu
-::q::
-menusec = gg.choice({
-"// Yes //",
-"// No //"
-}, nil, "Do you really want to Quit?")
-if menusec == nil then else
-if menusec == 1 then goto qy end
-if menusec == 2 then goto s end
+if menu == 4 then goto qy end
 end
 os.exit()
 
--- Script
 ::ims::
 TG = gg.makeRequest("https://github.com/mioscape/mioproject/raw/master/icon-radar-unity.lua").content
 if not TG then
-gg.alert('Turn on Internet!')
+gg.alert('Connectivity Problem')
 os.exit()
 else
 pcall(load(TG))
@@ -46,7 +32,7 @@ os.exit()
 ::nrs::
 TG = gg.makeRequest("https://github.com/mioscape/mioproject/raw/master/normal-radar-unity.lua").content
 if not TG then
-gg.alert('Turn on Internet!')
+gg.alert('Connectivity Problem')
 os.exit()
 else
 pcall(load(TG))
@@ -56,16 +42,11 @@ os.exit()
 ::beta::
 TG = gg.makeRequest("https://raw.githubusercontent.com/mioscape/mioproject/master/radar-beta.lua").content
 if not TG then
-gg.alert('Turn on Internet!')
+gg.alert('Connectivity Problem')
 os.exit()
 else
 pcall(load(TG))
 end
-os.exit()
-
-::su::
-gg.alert('Donate Pulsa to Mio Natsume :)\nIt will help Mio a lot')
-goto s
 os.exit()
 
 ::qy::

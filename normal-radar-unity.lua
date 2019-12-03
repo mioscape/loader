7d2f073a94b2b09a903aced278755a2decbaccc7
Mioscape <43681888+mioscape@users.noreply.github.com>
2019-12-03 11:31:52 +0700

    normal-radar-unity (Under Development)
    Sync with mioscape source v2.6.1

--- normal-radar-unity.lua
+++ normal-radar-unity.lua
@@ -2,7 +2,7 @@
 -- Menu
 TYU=1
 function START()
-ST=gg.choice({'[1️⃣] Radar 美緒 (In Game)','[2️⃣] Drone 美緒 (Draft Pick / Pick Hero)','[3️⃣] Credit','[4️⃣] Quit'}, nil, "The version you have is mioproject Script v2.5stablenormalonly")
+ST=gg.choice({'[1️⃣] Radar 美緒 (In Game)','[2️⃣] Drone 美緒 (Lobby)','[3️⃣] Credit','[4️⃣] Quit'}, nil, "The version you have is mioproject Script v2.6.1stablenormalonly")
 if ST == 1 then radar() end
 if ST == 2 then drone() end
 if ST == 3 then credit() end
@@ -22,64 +22,27 @@ end
 
 TYU=1
 function drone()
-DR=gg.choice({"[1️⃣] Default","[2️⃣] Medium","[3️⃣] High","[4️⃣] Back"}, nil, "Drone Menu!")
-if DR == 1 then dd() end
-if DR == 2 then dm() end
-if DR == 3 then dh() end
-if DR == 4 then START() end
+DR=gg.choice({"[2️⃣] Medium","[3️⃣] High","[4️⃣] Back"}, nil, "Drone Menu!")
+if DR == 1 then dm() end
+if DR == 2 then dh() end
+if DR == 3 then START() end
 TYU=-1
 end
 
 -- Number Code
 function nr()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.clearResults()
-gg.searchNumber("14;-1;65,536::9", gg.TYPE_DWORD)
-gg.searchNumber("14", gg.TYPE_DWORD)
-gg.getResults(200)
-gg.editAll('9', gg.TYPE_DWORD)
-local t = gg.getResults(9999)
-for i, v in ipairs(t) do
-	if v.flags == gg.TYPE_DWORD then
-		v.value = "9"
-		v.freeze = true
-	end
-end
-gg.addListItems(t)
-t = nil
-gg.clearResults()
-gg.toast('Normal Radar Activated!')
-end
+function split(szFullString, szSeparator) local nFindStartIndex = 1 local nSplitIndex = 1 local nSplitArray = {} while true do local nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex) if not nFindLastIndex then nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString)) break end nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1) nFindStartIndex = nFindLastIndex + string.len(szSeparator) nSplitIndex = nSplitIndex + 1 end return nSplitArray end 
+function xgxc(szpy, mio) for x = 1, #(mio) do xgpy = szpy + mio[x]['offset'] xglx = mio[x]['type'] xgsz = mio[x]['value'] xgdj = mio[x]['freeze'] if xgdj == nil or xgdj == '' then gg.setValues({[1] = {address = xgpy, flags = xglx, value = xgsz}}) else gg.addListItems({[1] = {address = xgpy, flags = xglx, freeze = xgdj, value = xgsz}}) end xgsl = xgsl + 1 xgjg = true end end 
+function xzn(ze) gg.clearResults() gg.setRanges(ze[1]['memory']) gg.searchNumber(ze[3]['value'], ze[3]['type']) if gg.getResultCount() == 0 then gg.toast(ze[2]['name'] .. 'Success') else gg.refineNumber(ze[3]['value'], ze[3]['type']) gg.refineNumber(ze[3]['value'], ze[3]['type']) gg.refineNumber(ze[3]['value'], ze[3]['type']) if gg.getResultCount() == 0 then gg.toast(ze[2]['name'] .. 'Success') else sl = gg.getResults(999999) sz = gg.getResultCount() xgsl = 0 if sz > 999999 then sz = 999999 end for i = 1, sz do pdsz = true for v = 4, #(ze) do if pdsz == true then pysz = {} pysz[1] = {} pysz[1].address = sl[i].address + ze[v]['offset'] pysz[1].flags = ze[v]['type'] szpy = gg.getValues(pysz) pdpd = ze[v]['lv'] .. ';' .. szpy[1].value szpd = split(pdpd, ';') tzszpd = szpd[1] pyszpd = szpd[2] if tzszpd == pyszpd then pdjg = true pdsz = true else pdjg = false pdsz = false end end end if pdjg == true then szpy = sl[i].address xgxc(szpy, mio) end end if xgjg == true then gg.toast(ze[2]['name'] .. 'Radar ON') else gg.toast(ze[2]['name'] .. 'Success') end end end end
 
-function dd()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.clearResults()
-gg.searchNumber('1089806008;-1053839852;1089722122', gg.TYPE_DWORD)
-gg.searchNumber('1089806008', gg.TYPE_DWORD)
-gg.getResults(100)
-gg.editAll('1092616192', gg.TYPE_DWORD)
-gg.clearResults()
-gg.searchNumber('1092616192;-1053839852;1089722122', gg.TYPE_DWORD)
-gg.searchNumber('-1053839852', gg.TYPE_DWORD)
-gg.getResults(100)
-gg.editAll('-1050620723', gg.TYPE_DWORD)
-gg.clearResults()
-gg.searchNumber('1092616192;-1050620723;1089722122', gg.TYPE_DWORD)
-gg.searchNumber('1089722122', gg.TYPE_DWORD)
-gg.getResults(100)
-gg.editAll('1092584735', gg.TYPE_DWORD)
-gg.clearResults()
-gg.searchNumber('-1057677640;-1057761526;1110143140', gg.TYPE_DWORD)
-gg.searchNumber('-1057677640', gg.TYPE_DWORD)
-gg.getResults(100)
-gg.editAll('-1054867456', gg.TYPE_DWORD)
-gg.clearResults()
-gg.searchNumber('-1054867456;-1057761526;1110143140', gg.TYPE_DWORD)
-gg.searchNumber('-1057761526', gg.TYPE_DWORD)
-gg.getResults(100)
-gg.editAll('-1054898913', gg.TYPE_DWORD)
-gg.clearResults()
-gg.toast("Activated!")
+ze ={{['memory']=32},
+{['name']=''},
+{['value']=2.53125,['type']=16},
+{['lv']=2.3693558e-38,['offset']=0x4,['type']=16},
+{['lv']=0,['offset']=0x19,['type']=4},}
+mio ={{['value']=1,['offset']=0x18,['type']=4,['freeze']=true},}
+xzn(ze)
+gg.toast('Normal Radar Activated!')
 end
 
 function dm()

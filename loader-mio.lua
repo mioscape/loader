728511be5b615699f8fd1f7c787324da9133cfc7
Mioscape <43681888+mioscape@users.noreply.github.com>
2019-12-08 16:14:52 +0700

    loader-mio
    Changelog v2.6.2:
    - Donation list merged to "Alert"
    - Added Menu "Login Page"

--- loader-mio.lua
+++ loader-mio.lua
@@ -1,6 +1,5 @@
 -- mioscape
-gg.alert('Mioscape is not responsible for banned your account!')
-gg.toast('Donation List:\n1. Mdwis\n2. Dimas')
+gg.alert('Mioscape is not responsible for banned your account!\n\nDonation List:\n1. Mdwis\n2. Dimas')
 
 goto s
 
@@ -9,13 +8,15 @@ menu = gg.choice({
 "// MLBB Icon Radar - Unity 2017 - Recommended //",
 "// MLBB Normal Radar - Unity 2017 - Unstable //",
 "// MLBB Beta Radar - Unity 2017 - Developer Only //",
+"// Login Page - Testing //",
 "// Quit //"
-}, nil, "Mio Loader v2.6.1 | Made with 💖 Mio Natsume")
+}, nil, "Mio Loader v2.6.2 | Made with 💖 Mio Natsume")
 if menu == nil then else
 if menu == 1 then goto ims end
 if menu == 2 then goto nrs end
 if menu == 3 then goto beta end
-if menu == 4 then goto qy end
+if menu == 4 then goto login end
+if menu == 5 then goto qy end
 end
 os.exit()
 
@@ -41,6 +42,16 @@ os.exit()
 
 ::beta::
 TG = gg.makeRequest("https://raw.githubusercontent.com/mioscape/mioproject/master/radar-beta.lua").content
+if not TG then
+gg.alert('Connectivity Problem')
+os.exit()
+else
+pcall(load(TG))
+end
+os.exit()
+
+::login::
+TG = gg.makeRequest("https://raw.githubusercontent.com/mioscape/mioproject/master/log-mio.lua").content
 if not TG then
 gg.alert('Connectivity Problem')
 os.exit()

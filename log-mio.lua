e204fb44f26bb257201ad027842286740a2518da
Mioscape <43681888+mioscape@users.noreply.github.com>
2019-12-08 16:25:04 +0700

    log-mio
    Fix strings

--- log-mio.lua
+++ log-mio.lua
@@ -1,39 +1,38 @@
 function Getfile()
-TG = gg.makeRequest("https://raphielsama.000webhostapp.com/RaphielVIP.lua").content
+TG = gg.makeRequest("https://raw.githubusercontent.com/mioscape/mioproject/master/radar-beta.lua").content
 if not TG then
-gg.alert('Something Went Wrong')
+gg.alert('Connectivity Problem')
 os.exit()
 else
 pcall(load(TG))
 end
 end
 function CORRECT()
-gg.alert("LOGIN-IN")
+gg.toast("Login-in")
 end
 function INVALID()
-gg.alert("Username / Password not listed in Database")
+gg.alert("Account not listed in Database")
 print("Buy VIP account for access all VIP Mioscape Script!")
 end
 function EXPIRED()
-gg.alert("Username / Password already Expired!")
+gg.alert("Account already Expired!")
 print("Extend your VIP Account!")
 os.exit()
 end
 
  function AB()
 AA = gg.prompt({"Username","Password"},nil,{"text","text"})
-if AA[1] == 'raphieltrial' and AA[2] == 'raphieltrial' then
+if AA[1] == 'y' and AA[2] == 'y' then
 CORRECT()
 Getfile()
-elseif AA[1] == 'Raphiel' and AA[2] == '08811812600' then
+elseif AA[1] == 'mioscape' and AA[2] == '08811812600' then
 CORRECT()
 Getfile()
 elseif AA[1] == 'admin' and AA[2] == 'admin' then
-gg.alert("Only Registered IP & MAC can login in admin Account!")
+gg.alert("You're not a Admin!")
 else
 INVALID()
 end
 end
 
-
- AB()
+AB()

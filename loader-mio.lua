f31d5faa99eff4331fc537b85cacaba8bdc251c2
Mioscape <43681888+mioscape@users.noreply.github.com>
2019-12-27 19:39:23 +0700

    a

--- loader-mio.lua
+++ loader-mio.lua
@@ -1,23 +1,21 @@
 -- mioscape
-gg.alert('Mioscape is not responsible for banned your account!\n\nDonation List:\n1. Mdwis\n2. Dimas')
+gg.alert('Mioscape is not responsible for banned your account!\n\nDonation List:\n1. Mdwis\n2. Dimas\n3. Adam R Iskandar\n\n**Nama Donatur yang di Tampilkan diambil dari Username Whatsapp.')
 
 MIO=1
 function START()
   ST=gg.choice({
     '// MLBB Icon Radar - v2.6.4stable //',
     '// MLBB Normal Radar - v0.4-2412191804+GMT7alpha //',
-    '// MLBB Beta Radar - v2.7beta //',
     '// Quit //'
-}, nil, 'Mio Loader v2.6.4 | Made with 💖 Mio Natsume')
+}, nil, 'Mio Loader v2.6.5 | Made with 💖 Mio Natsume')
 if ST == 1 then ims() end
 if ST == 2 then nrs() end
-if ST == 3 then beta() end
-if ST == 4 then qy() end
+if ST == 3 then qy() end
 MIO=-1
 end
 
 function ims()
-  TG = gg.makeRequest('https://github.com/mioscape/mioproject/raw/master/icon-radar-unity.lua').content
+  TG = gg.makeRequest('https://github.com/mioscape/project/raw/master/icon-radar.lua').content
   if not TG then
   gg.alert('Connectivity Problem')
   os.exit()
@@ -27,17 +25,7 @@ function ims()
 end
 
 function nrs()
-  TG = gg.makeRequest('https://github.com/mioscape/mioproject/raw/master/normal-radar-unity.lua').content
-  if not TG then
-  gg.alert('Connectivity Problem')
-  os.exit()
-  else
-  pcall(load(TG))
-  end
-end
-
-function beta()
-  TG = gg.makeRequest('https://raw.githubusercontent.com/mioscape/mioproject/master/radar-beta.lua').content
+  TG = gg.makeRequest('https://github.com/mioscape/project/raw/master/normal-radar.lua').content
   if not TG then
   gg.alert('Connectivity Problem')
   os.exit()

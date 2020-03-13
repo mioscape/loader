5f5b632bcfddac9fe7b7044bffda245e9e47b9b2
mioscape <mioscape@gmail.com>
2020-03-14 02:55:38 +0700

    wip-radar v0.3.0253-alpha; Changelogs: - Added New Value; Old still Saved for Testing Purpose - Fix Strings - Cut useless Text
    //

--- wip-radar.lua
+++ wip-radar.lua
@@ -1,9 +1,8 @@
--- mioscape
 MIO=1
 function START()
   ST=gg.choice({
     '\n[Radar 美緒]\n',
-}, nil, '[WIP Radar v0.1.0355-alpha | Made By Mioscape with 💖]')
+}, nil, '[WIP Radar v0.3.0253-alpha | Made By Mioscape with 💖]')
 if ST == 1 then rd() end
 MIO=-1
 end
@@ -11,15 +10,17 @@ end
 MIO=1
 function rd()
   RD=gg.choice({
-    '\n[Jungle]\n',
-    '\n[Icon]\n',
+    '\n[Jungle | Stable]\n',
+    '\n[Jungle | Alpha]\n',
+    '\n[Icon | Stable]\n',
     '\n[Revert]\n',
     '\n[Back]\n'
 }, nil, '')
 if RD == 1 then ta() end
 if RD == 2 then tb() end
-if RD == 3 then rv() end
-if RD == 4 then START() end
+if RD == 3 then is() end
+if RD == 4 then rv() end
+if RD == 5 then START() end
 MIO=-1
 end
 
@@ -43,6 +44,25 @@ function ta()
 end
 
 function tb()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.clearResults()
+  gg.searchNumber('256D;1D;0D;0D;0F;0D::21', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.processResume()
+  gg.refineNumber('1', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  local t = gg.getResults(100)
+  for i, v in ipairs(t) do
+  if v.flags == gg.TYPE_DWORD then
+    v.value = '2'
+    v.freeze = true
+   end
+  end
+  gg.addListItems(t)
+  t = nil
+  gg.clearResults()
+  gg.toast('Activated')
+end
+
+function is()
   gg.setRanges(gg.REGION_ANONYMOUS)
   gg.clearResults()
   gg.searchNumber('2.53125F;2.3693558e-38F;0F;0F;0F;0F;0D::25', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)

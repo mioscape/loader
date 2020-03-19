aaf5d0f3b284d69c2a9dd9ef7cc1179ff25df1dc
mioscape <mioscape@gmail.com>
2020-03-20 02:13:07 +0700

    nfsnl\beta v0.6+1-beta; Changelogs: - Removed DEBUG Menu - Removes Codes that Connected to DEBUG Menu
    //

--- nfsnl/beta.lua
+++ nfsnl/beta.lua
@@ -4,26 +4,11 @@ function START()
     '\n[Unlock Cars]\n',
     '\n[Money]\n',
     '\n[Gold]\n',
-    '\n[Debug]\n',
-}, nil, '[NFS:NL v0.4+3-beta | Made By Mioscape with 💖]')
+}, nil, '[NFS:NL v0.6+1-beta | Made By Mioscape with 💖]')
 if ST == 1 then ab() end
 if ST == 2 then cd() end
 if ST == 3 then ef() end
-if ST == 4 then gh() end
 MIO=-1
-end
-
-MIO=1
-function gh()
-  DEBUG=gg.choice({
-    '\n[Pause Game]\n',
-    '\n[Resume Game]\n',
-    '\n[Back]\n',
-  }, nil, '')
-  if DEBUG == 1 then pg() end
-  if DEBUG == 2 then rg() end
-  if DEBUG == 3 then START() end
-  MIO=-1
 end
 
 function ab()
@@ -61,16 +46,6 @@ function ef()
   gg.editAll(mio[2], gg.TYPE_DWORD)
   gg.processResume()
   gg.toast('Gold Changed')
-end
-
-function pg()
-  gg.processPause()
-  gg.toast('Game Paused')
-end
-
-function rg()
-  gg.processResume()
-  gg.toast('Game Resumed')
 end
 
 while true do

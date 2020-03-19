a9ff4ddc0787545877ff8a2a6dfb9ccdbc66a82d
mioscape <mioscape@gmail.com>
2020-03-20 02:08:27 +0700

    nfsnl\beta v0.4+3-beta; Changelogs: - Added DEBUG Menu - Fix Strings - Fix Codes
    //

--- nfsnl/beta.lua
+++ nfsnl/beta.lua
@@ -1,14 +1,29 @@
 MIO=1
 function START()
   ST=gg.choice({
-    '\n[Unlock Car]\n',
+    '\n[Unlock Cars]\n',
     '\n[Money]\n',
     '\n[Gold]\n',
-}, nil, '[NFS:NL v0.3+4-beta | Made By Mioscape with 💖]')
+    '\n[Debug]\n',
+}, nil, '[NFS:NL v0.4+3-beta | Made By Mioscape with 💖]')
 if ST == 1 then ab() end
 if ST == 2 then cd() end
 if ST == 3 then ef() end
+if ST == 4 then gh() end
 MIO=-1
+end
+
+MIO=1
+function gh()
+  DEBUG=gg.choice({
+    '\n[Pause Game]\n',
+    '\n[Resume Game]\n',
+    '\n[Back]\n',
+  }, nil, '')
+  if DEBUG == 1 then pg() end
+  if DEBUG == 2 then rg() end
+  if DEBUG == 3 then START() end
+  MIO=-1
 end
 
 function ab()
@@ -27,7 +42,7 @@ end
 function cd()
   gg.processPause()
   gg.setRanges(gg.REGION_C_ALLOC)
-  mio = gg.prompt({[1]='Your Money', [2]='Change Money'}, {[1]='', [2]=''})
+  mio = gg.prompt({[1]='Your Money:', [2]='Change Money:'}, {[1]='', [2]=''})
   gg.searchNumber(mio[1]..';327680~327699::5', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)
   gg.getResults(100)
@@ -39,13 +54,23 @@ end
 function ef()
   gg.processPause()
   gg.setRanges(gg.REGION_C_ALLOC)
-  mio = gg.prompt({[1]='Your Gold', [2]='Change Gold'}, {[1]='', [2]=''})
+  mio = gg.prompt({[1]='Your Gold:', [2]='Change Gold:'}, {[1]='', [2]=''})
   gg.searchNumber(mio[1]..';327680~327699::5', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)
   gg.getResults(100)
   gg.editAll(mio[2], gg.TYPE_DWORD)
   gg.processResume()
   gg.toast('Gold Changed')
+end
+
+function pg()
+  gg.processPause()
+  gg.toast('Game Paused')
+end
+
+function rg()
+  gg.processResume()
+  gg.toast('Game Resumed')
 end
 
 while true do

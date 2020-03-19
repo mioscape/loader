189e2ee89ec1b5a95123a660ad9f3203b40d1b75
mioscape <mioscape@gmail.com>
2020-03-20 00:08:00 +0700

    nfsnl\wip v0.3.0007-alpha; Changelogs: - Changed Ranges Memory
    //

--- nfsnl/wip.lua
+++ nfsnl/wip.lua
@@ -4,7 +4,7 @@ function START()
     '\n[Unlock Car]\n',
     '\n[Money]\n',
     '\n[Gold]\n',
-}, nil, '[NFS:NL WIP v0.1-alpha | Made By Mioscape with 💖]')
+}, nil, '[NFS:NL WIP v0.3.0007-alpha | Made By Mioscape with 💖]')
 if ST == 1 then ab() end
 if ST == 2 then cd() end
 if ST == 3 then ef() end
@@ -12,8 +12,8 @@ MIO=-1
 end
 
 function ab()
+  gg.setRanges(gg.REGION_JAVA_HEAP, REGION_C_HEAP, REGION_C_ALLOC, REGION_C_DATA, REGION_C_BSS, REGION_PPSSPP, gg.REGION_ANONYMOUS)
   gg.processPause()
-  gg.setRanges(gg.REGION_ANONYMOUS)
   gg.clearResults()
   gg.searchNumber('0;327700~327710;24~26::9', gg.TYPE_DWORD)
   gg.refineNumber('0', gg.TYPE_DWORD)
@@ -29,6 +29,7 @@ function cd()
 end
 
 function ef()
+  gg.setRanges(gg.REGION_JAVA_HEAP, REGION_C_HEAP, REGION_C_ALLOC, REGION_C_DATA, REGION_C_BSS, REGION_PPSSPP, gg.REGION_ANONYMOUS)
   gg.processPause()
   data = gg.prompt({[1]='Your Gold-1 (Change * to your Gold)', [2]='Your Gold-2', [3]='Change Gold'}, {[1]='*;327680~327699::5', [2]='', [3]=''})
   gg.searchNumber(data[1], gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)

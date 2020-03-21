fe114b513dd25c1b5c3cf019bf9b95f5c08ec6c6
mioscape <mioscape@gmail.com>
2020-03-21 23:42:31 +0700

    nfsnl\stable v1.0+1-stable; Changelogs: - Staging to Stable Release - Fix Codes of ab() - Added Level & Material Ammount Features
    //

--- nfsnl/stable.lua
+++ nfsnl/stable.lua
@@ -4,10 +4,14 @@ function START()
     '\n[Unlock Cars]\n',
     '\n[Money]\n',
     '\n[Gold]\n',
-}, nil, '[NFS:NL v0.6+1-beta | Made By Mioscape with 💖]')
+    '\n[Level]\n',
+    '\n[Material Amount]\n',
+}, nil, '[NFS:NL v1.0+1-stable | Made By Mioscape with 💖]')
 if ST == 1 then ab() end
 if ST == 2 then cd() end
 if ST == 3 then ef() end
+if ST == 4 then gh() end
+if ST == 5 then ij() end
 MIO=-1
 end
 
@@ -17,7 +21,7 @@ function ab()
   gg.clearResults()
   gg.searchNumber('0;327700~327710;24~26::9', gg.TYPE_DWORD)
   gg.refineNumber('0', gg.TYPE_DWORD)
-  gg.getResults(100)
+  gg.getResults(1000)
   gg.editAll('3638850', gg.TYPE_DWORD)
   gg.clearResults()
   gg.processResume()
@@ -46,6 +50,30 @@ function ef()
   gg.editAll(mio[2], gg.TYPE_DWORD)
   gg.processResume()
   gg.toast('Gold Changed')
+end
+
+function gh()
+  gg.processPause()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  mio = gg.prompt({[1]='Your Level:', [2]='Change Level (Max 500):'}, {[1]='', [2]='500'})
+  gg.searchNumber(mio[1]..';327703~327705::128', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.refineNumber(mio[1], gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.editAll(mio[2], gg.TYPE_DWORD)
+  gg.processResume()
+  gg.toast('Level Changed')
+end
+
+function ij()
+  gg.processPause()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  mio = gg.prompt({[1]='Your Material Amount:', [2]='Change Material Amount:'}, {[1]='', [2]=''})
+  gg.searchNumber(mio[1]..';327691;26;262144;2621443::21', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.refineNumber(mio[1], gg.TYPE_DWORD)
+  gg.getResults(1000)
+  gg.editAll(mio[2], gg.TYPE_DWORD)
+  gg.processResume()
+  gg.toast('Material Amount Changed')
 end
 
 while true do

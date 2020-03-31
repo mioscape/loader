e941661a55216d34f31470b25fe0fd3c9ddb798c
mioscape <mioscape@gmail.com>
2020-04-01 03:10:24 +0700

    nfsnl\stable v1.4+2-stable; Changelogs: - Fix Codes - Fix Strings
    //

--- nfsnl/stable.lua
+++ nfsnl/stable.lua
@@ -1,3 +1,4 @@
+gg.setRanges(gg.REGION_C_ALLOC)
 MIO=1
 function START()
   ST=gg.choice({
@@ -5,7 +6,7 @@ function START()
     '\n[Money]\n',
     '\n[Gold]\n',
     '\n[Material Amount]\n',
-}, nil, '[NFS:NL v1.3+5-stable | Made By Mioscape with 💖]')
+}, nil, '[NFS:NL v1.4+2-stable | Made By Mioscape with 💖]')
 if ST == 1 then ab() end
 if ST == 2 then cd() end
 if ST == 3 then ef() end
@@ -15,7 +16,6 @@ end
 
 function ab()
   gg.processPause()
-  gg.setRanges(gg.REGION_C_ALLOC)
   mio = gg.prompt({[1]='Your Unlocked Car:'}, {[1]=''})
   gg.searchNumber(mio[1]..';327700~327710;24~26::9', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)
@@ -28,7 +28,6 @@ end
 
 function cd()
   gg.processPause()
-  gg.setRanges(gg.REGION_C_ALLOC)
   mio = gg.prompt({[1]='Your Money:', [2]='Change Money:'}, {[1]='', [2]=''})
   gg.searchNumber(mio[1]..';327680~327699::5', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)
@@ -41,7 +40,6 @@ end
 
 function ef()
   gg.processPause()
-  gg.setRanges(gg.REGION_C_ALLOC)
   mio = gg.prompt({[1]='Your Gold:', [2]='Change Gold:'}, {[1]='', [2]=''})
   gg.searchNumber(mio[1]..';327680~327699::5', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)
@@ -54,7 +52,6 @@ end
 
 function gh()
   gg.processPause()
-  gg.setRanges(gg.REGION_C_ALLOC)
   mio = gg.prompt({[1]='Your Material Amount:', [2]='Change Material Amount:'}, {[1]='', [2]=''})
   gg.searchNumber(mio[1]..';327691;26;262144;2621443::21', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.refineNumber(mio[1], gg.TYPE_DWORD)

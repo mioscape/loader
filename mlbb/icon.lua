4f1458dd58162491e9dff102a7a6ae631b609f3f
mioscape <mioscape@gmail.com>
2020-05-17 01:40:03 +0700

    mlbb\icon v3.6+5-test; Changelogs: - Strings Changes - Value Changes - Bump to v3.6+5-test |

--- mlbb/icon.lua
+++ mlbb/icon.lua
@@ -2,10 +2,10 @@ gg.setRanges(gg.REGION_ANONYMOUS)
 MIO=1
 function mioscape()
   ST=gg.choice({
-    '\n[  Radar ]\n',
-    '\n[  Drone ]\n',
-    '\n[  Exit  ]\n',
-}, nil, '[Icon Radar v3.5+3-test | Made By Mioscape with 💖]')
+    '\n[ Radar ]\n',
+    '\n[ Drone ]\n',
+    '\n[ Exit ]\n',
+}, nil, '[Icon Radar v3.6+5-test | Made By Mioscape with 💖]')
 if ST == 1 then rd() end
 if ST == 2 then dr() end
 if ST == 3 then qu() end
@@ -15,9 +15,9 @@ end
 MIO=1
 function rd()
   RD=gg.choice({
-    '\n[  Icon Radar  ]\n',
-    '\n[  Revert  ]\n',
-    '\n[  Back  ]\n'
+    '\n[ Icon Radar ]\n',
+    '\n[ Revert ]\n',
+    '\n[ Back ]\n'
 }, nil, '')
 if RD == 1 then ir() end
 if RD == 2 then rr() end
@@ -28,9 +28,9 @@ end
 MIO=1
 function dr()
   DR=gg.choice({
-    '\n[  Medium  ]\n',
-    '\n[  High  ]\n',
-    '\n[  Back  ]\n'
+    '\n[ Medium ]\n',
+    '\n[ High ]\n',
+    '\n[ Back ]\n'
 }, nil, '')
 if DR == 1 then dm() end
 if DR == 2 then dh() end
@@ -54,7 +54,7 @@ gg.refineNumber('0', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
 local t = gg.getResults(100)
 for i, v in ipairs(t) do
 if v.flags == gg.TYPE_DWORD then
-  v.value = '-1'
+  v.value = '3105'
   v.freeze = true
  end
 end

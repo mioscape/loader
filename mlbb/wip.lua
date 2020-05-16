9b401f07574c62c7095003bdb5ad2141ea9c35ad
mioscape <mioscape@gmail.com>
2020-05-17 05:49:56 +0700

    mlbb\wip v0.23.0549-alpha; Changelogs: - Added Exit Button - Value Changes |

--- mlbb/wip.lua
+++ mlbb/wip.lua
@@ -2,8 +2,10 @@ MIO=1
 function START()
   ST=gg.choice({
     '\n[Radar 美緒]\n',
-}, nil, '[WIP Radar v0.22.0514-alpha | Made By Mioscape with 💖]')
+    '\n[Exit]\n',
+}, nil, '[WIP Radar v0.23.0549-alpha | Made By Mioscape with 💖]')
 if ST == 1 then rd() end
+if ST == 2 then ex() end
 MIO=-1
 end
 
@@ -28,7 +30,7 @@ function ra()
   gg.getResults(100)
   gg.editAll("h 00 68 43 B9 1F 14 00 71 21 03 00 54 1F 0C 00 71", gg.TYPE_BYTE)
   gg.clearResults()
-  gg.searchNumber("h 00 65 43 B9 1F 14 00 71 21 03 00 54 1F 0C 00 71 ", gg.TYPE_BYTE)
+  gg.searchNumber("h 00 65 43 B9 1F 14 00 71 21 03 00 54 1F 0C 00 71", gg.TYPE_BYTE)
   gg.getResults(100)
   gg.editAll("h 00 68 43 B9 1F 14 00 71 21 03 00 54 1F 0C 00 71", gg.TYPE_BYTE)
   gg.clearResults()
@@ -38,6 +40,10 @@ function ra()
   gg.editAll("h C0 03 5F D6 20 00 80 D2 C0 03 5F D6 FD 7B 02 A9", gg.TYPE_BYTE)
   gg.clearResults()
   gg.toast('Activated')
+end
+
+function ex()
+os.exit()
 end
 
 function rv()
